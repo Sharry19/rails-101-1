dfc9483b82b4040d13723d91a20c97faf10eee16
Sharry <shen.sharry@gmail.com>
2017-11-13 15:42:50 +0800

    check out

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -1,7 +1,7 @@
 class Account::GroupsController < ApplicationController
   before_action :authenticate_user!
 
-  def index
+ def index
    @groups = current_user.participated_groups
-  end
+ end
 end
